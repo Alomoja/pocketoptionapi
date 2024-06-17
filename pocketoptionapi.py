1c58781c1f1aeb8866bdfbd8fe02deb2472cd780
Alomoja <168875439+Alomoja@users.noreply.github.com>
2024-06-17 05:55:12 -0700

    Update pocketoptionapi.py

--- pocketoptionapi.py
+++ pocketoptionapi.py
@@ -1,6 +1,6 @@
 from pocketoptionapi import PocketOption
 
-ssid = "0{"sid":"Oiri0O_zQMWfnyVuGA86","upgrades":[],"pingInterval":25000,"pingTimeout":20000,"maxPayload":1000000}"
+ssid = "0{"sid":"G5mdKWqlLwD-SV_RAF4J","upgrades":[],"pingInterval":25000,"pingTimeout":20000,"maxPayload":1000000}	"
 account = PocketOption(ssid)
 account.connect()
 balance = account.get_balance()
